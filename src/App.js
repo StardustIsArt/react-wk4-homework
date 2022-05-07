cdd7bc1d89d4bda7bdfc55e3eb99a19393752a01
StardustIsArt <yocius_sa@icloud.com>
2022-05-07 17:07:45 -0500

    added contact info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    React Weather App
+      React Weather App
+      
+      
+      <footer className="footer">
+        <h5>
+          This project was coded by:
+          <a
+            href="https://yociusefx.myportfolio.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Samantha Yocius
+          </a>{" "} &
+          Openly sourced on:
+          <a
+            href="https://github.com/StardustIsArt/react-wk4-homework"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            GitHub
+          </a>
+        </h5>
+      </footer>
     </div>
   );
 }
