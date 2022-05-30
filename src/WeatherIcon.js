bba18644ca9033e31ea70803a421d2d5ed993715
StardustIsArt <yocius_sa@icloud.com>
2022-05-30 09:51:29 -0500

    trying to fix show icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -7,16 +7,16 @@ export default function WeatherIcon(props) {
     "01n": "CLEAR_NIGHT",
     "02d": "PARTLY_CLOUDY_DAY",
     "02n": "PARTLY_CLOUDY_NIGHT",
-    "03d": "SCATTERED_CLOUDS_DAY",
-    "03n": "SCATTERED_CLOUDS_NIGHT",
-    "04d": "PARTLY_CLOUDY",
-    "04n": "PARTLY_CLOUDY",
-    "09d": "RAN SHOWERS",
-    "09n": "RAIN SHOWERS",
+    "03d": "PARTLY_CLOUDY_DAY",
+    "03n": "PARTLY_CLOUDY_NIGHT",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "RAIN",
+    "09n": "RAIN",
     "10d": "RAIN",
     "10n": "RAIN",
-    "11d": "THUNDERSTORMS",
-    "11n": "THUNDERSTORMS",
+    "11d": "RAIN",
+    "11n": "RAIN",
     "13d": "SNOW",
     "13n": "SNOW",
     "50d": "FOG",
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#1e1e1e1e"
+      color="#1e1e1e"
       size={props.size}
       animate={true}
     />
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,9 +17,8 @@ export default function WeatherInfo(props) {
         <div className="col-7">
           <div className="clearfix">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={10} />
+              <WeatherIcon code={props.data.icon} size={52} />
               <WeatherTemperature celsius={props.data.temperature} />
-              
             </div>
           </div>
         </div>
